bb6daf06901c168798ed0ded5f39f86c92abc2d0
Mogrima <rebel.rs@yandex.ru>
2019-03-17 22:48:03 +0500

    Сам скрипт для работы табов

---
+++ js/script.js
@@ -0,0 +1,40 @@
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
+    let tab = document.querySelectorAll('.tab__toggle');
+    let info = document.querySelector('.tab__toggles');
+    let tabContent = document.querySelectorAll('.tab__content');
+
+    console.log(tab);
+    console.log(info);
+    console.log(tabContent);
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('tab__toggle')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+});
